cc3e8f58957c388a3e14eb7e67d4d6f4e84cbddb
Soheil Mohseni <soheil07118@gmail.com>
2025-12-01 17:10:53 +0330

    FEAT:get instruction in REPL mode

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let mut command  = String::new();
-    let _ = stdin().read_line(&mut command).unwrap();
-    eprintln!("{}: command not found",command.trim());
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut command = String::new();
+        let _ = stdin().read_line(&mut command).unwrap();
+        eprintln!("{}: command not found", command.trim());
+    }
 }

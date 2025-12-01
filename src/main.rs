af055001f3e86b0557857a9ee851ce60c600a963
Soheil Mohseni <soheil07118@gmail.com>
2025-12-01 17:01:55 +0330

    chore : get user input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::io::stdin;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
-    // io::stdout().flush().unwrap();
+    let mut command  = String::new();
+    let _ = stdin().read_line(&mut command).unwrap();
+    eprintln!("{}: Invalid commadnd",command.trim());
+    io::stdout().flush().unwrap();
 }

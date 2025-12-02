42c5277b8e5eea61e02305a0c9e7b2e559663831
Soheil Mohseni <soheil07118@gmail.com>
2025-12-02 10:49:12 +0330

    FEAT: refactore general code and make engine & implement Echo and Exit command

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,51 @@
-use std::io::stdin;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::{collections::HashMap, io::stdin};
+
+struct Engine {
+    handlers: HashMap<&'static str, fn(&str)>,
+}
+
+// #[derive(Hash, Eq, PartialEq)]
+// enum Engine_method {
+//     echo,
+// }
+
+impl Engine {
+    fn new() -> Self {
+        let mut handlers: HashMap<&'static str, fn(&str)> = HashMap::new();
+
+        handlers.insert("echo", Engine::echo);
+        handlers.insert("exit", Engine::exit);
+
+        Self { handlers }
+    }
+    fn echo(command: &str) {
+        println!("{}", command);
+    }
+
+    fn exit(_: &str) {
+        std::process::exit(0);
+    }
+    fn dispatch(&self, cmd: &str, command_value: &str) {
+        if let Some(handler) = self.handlers.get(cmd) {
+            handler(command_value);
+        } else {
+            eprintln!("{} {}: command not found", cmd, command_value);
+        }
+    }
+}
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+    let engine_instance = Engine::new();
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-        let mut command = String::new();
-        let _ = stdin().read_line(&mut command).unwrap();
-        
-        if command.trim() == "exit" {
-            break;
-        }
-        eprintln!("{}: command not found", command.trim());
+        let mut input = String::new();
+        let _ = stdin().read_line(&mut input).unwrap();
+        let mut command_input = input.trim().splitn(2, " ");
+        let command = command_input.next().unwrap_or("");
+        let command_value: &str = command_input.next().unwrap_or("");
+        engine_instance.dispatch(command, command_value);
     }
 }
